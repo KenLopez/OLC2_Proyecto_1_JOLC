88602f0d552b42e2e66b1a7b45643b5fdcc78ccb
klopezus <85919035+klopezus@users.noreply.github.com>
2021-09-20 22:50:02 -0600

    textareas

--- flask/Site.py
+++ flask/Site.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
+@app.route("/Compile")
+def compile(message=''):
+    return render_template('index.html', message=message)
+
 @app.route("/")
-def home():
-    return render_template('index.html', message='xd')
+def index():
+    return redirect("Compile")
 
 if __name__ == '__main__':
     app.run(debug=True)
